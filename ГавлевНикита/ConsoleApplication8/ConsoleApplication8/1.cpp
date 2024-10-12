864a9d47c6fc5aba82caca31e053c2882f043e64
ARXlMED <bygavlev@mail.ru>
2024-10-12 15:09:22 +0300

    all_labs_without_block_diagram

---
+++ ГавлевНикита/ConsoleApplication8/ConsoleApplication8/1.cpp
@@ -0,0 +1,41 @@
+﻿//#include <iostream>
+//#include <cmath>
+//
+//using namespace std;
+//
+//bool check_prime_number(unsigned long long number_for_check)
+//{
+//	for (long long i = 2; i <= (number_for_check / 2); i++)
+//	{
+//		if (number_for_check % i == 0)
+//		{
+//			return false;
+//		}
+//	}
+//	return true;
+//}
+//
+//unsigned long long check_perfect_number(int base_number)
+//{
+//	bool result_of_prime = check_prime_number(pow(2, base_number) - 1);
+//	if (result_of_prime == true)
+//		return(pow(2, base_number - 1) * (pow(2, base_number) - 1));
+//	else
+//		return 0;
+//}
+//
+//int main()
+//{
+//	setlocale(LC_ALL, "RU");
+//	int user_base_perfect_number;
+//	cout << "Введите совершенное число начиная с которого вы хотите начать нахождение новых совершенных чисел ";
+//	cin >> user_base_perfect_number;
+//	int max_base_number = { 1000 };
+//	setlocale(LC_ALL, "RU");
+//	for (int i = 2; i < max_base_number; i++)
+//	{
+//		if (check_perfect_number(i) != 0 and check_perfect_number(i) > user_base_perfect_number)
+//			cout << "Число " << pow(2, i - 1) * (pow(2, i) - 1) << " совершенное" << endl;
+//	}
+//}
+
---
+++ ГавлевНикита/ConsoleApplication8/ConsoleApplication8/2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cmath>
+#include <complex>
+
+using namespace std;
+
+double division(int x1, int x2)
+{
+	return static_cast<double> (x1) / x2;
+}
+
+complex<double> division(complex<double> z1, complex<double> z2)
+{
+	return (z1 / z2);
+}
+
+void printcomplex(complex<double> z)
+{
+	double real = z.real();
+	double imag = z.imag();
+	if (imag >= 0)
+		cout << real << " + " << imag << "i";
+	else
+		cout << real << " - " << abs(imag) << "i";
+}
+
+int main()
+{
+	string answer;
+	setlocale(LC_ALL, "RU");
+	while (true)
+	{
+		cout << "≈сли вы хотите делить целые числа введите 0, если комплексные 1: ";
+		cin >> answer;
+		if (answer == "0")
+		{
+			int x1, x2;
+			cout << "¬ведите 1 и 2 число через пробел ";
+			cin >> x1 >> x2;
+			cout << division(x1, x2) << endl;
+		}
+		else if (answer == "1")
+		{
+			double real1, imag1, real2, imag2;
+			cout << "¬ведите 1 комплексное число в виде x + iy, где x - ваш первый ввод, а y - второй (раздел€ютс€ пробелом) ";
+			cin >> real1 >> imag1;
+			cout << "¬ведите 2 комплексное число в виде x + iy, где x - ваш первый ввод, а y - второй (раздел€ютс€ пробелом) ";
+			cin >> real2 >> imag2;
+			complex<double> result = division(complex<double>(real1, imag1), complex<double>(real2, imag2));
+			cout << "–езультат делени€ комплексных чисел: ";
+			printcomplex(result);
+			cout << endl;
+		}
+		else
+		{
+			cout << "¬ведено неккоректное значение" << endl;
+		}
+	}
+}
