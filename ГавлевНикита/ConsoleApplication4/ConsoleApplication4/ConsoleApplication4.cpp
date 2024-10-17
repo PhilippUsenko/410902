41e32ce15fd3e823b9d8b2fd75b778699bdd2fec
ARXlMED <bygavlev@mail.ru>
2024-10-17 22:43:22 +0300

    blok-diagram4laba

--- ГавлевНикита/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
+++ ГавлевНикита/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -8,25 +8,29 @@ int main()
     const int n = 10;
     int k[n];
     int newk [n];
-
+    int oddindex = 0;
+    int evenindex = n / 2;
     cout << "Если хотите ввести свои значения в массив, введите 'input' или массив будет заполнен числами от 1 до " << n << ": ";
     string answer;
     cin >> answer;
-    if (answer == "input") {
+    if (answer == "input")
+    {
         cout << "Введите элементы массива: ";
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             cin >> k[i];
         }
     }
-    else {
-        for (int i = 0; i < n; i++) {
+    else 
+    {
+        for (int i = 0; i < n; i++) 
+        {
             k[i] = i + 1;
         }
     }
-    int oddindex = 0;
-    int evenindex = n / 2;
-
-    for (int i = 0; i < n; i++) {
+   
+    for (int i = 0; i < n; i++) 
+    {
         if ((i + 1) % 2 == 1)
         {
             newk[oddindex] = k[i];
@@ -37,10 +41,10 @@ int main()
             newk[evenindex] = k[i];
             evenindex += 1;
         }
-}
-
+    }
     cout << "Преобразованный массив: ";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cout << newk[i] << " ";
     }
 }
