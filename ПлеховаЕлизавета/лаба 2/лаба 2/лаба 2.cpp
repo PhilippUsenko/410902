5e03cde5751195d6732678e46737ced925fe5510
Elizaveta <sppunerizik@gmail.com>
2024-10-18 14:43:53 +0300

    Add files via upload
    добавлена 2 лаба

---
+++ ПлеховаЕлизавета/лаба 2/лаба 2/лаба 2.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std;
+
+
+int main()
+
+{
+    int n;
+    double x;
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите значение n" << endl;
+    cin >> n;
+    x = -1 / 2;
+    for (int i = 1; i = n; i++) x = x + (n * n * n / (n * (n + 1)));
+    cout << "Значение выражения равно" << x;
+    return 0;
+}
+
+
+
+
