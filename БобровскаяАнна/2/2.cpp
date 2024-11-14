8946abef50bd824003be977d76ab8ccb2261ae6e
bobrovskayaanya <annabobrovskaa53gmail.com>
2024-11-14 13:18:54 +0300

    laba2

---
+++ БобровскаяАнна/2/2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите 3 числа" << endl;
+	cin >> a >> b >> c;
+	if (a > c && a > b)
+		cout << "Наибольшее число: " << a;
+	else
+		if (b > c)
+			cout << "Наибольшее число: " << b;
+		else
+			cout << "Наибольшее число: " << c;
+	return 0;
+}
