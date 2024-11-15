a05317fbae3f0a0a6315083ce46456e661883b4a
DMila-522 <dmila8888mila@gmail.com>
2024-11-15 14:13:58 +0300

    3laba ready

---
+++ ДержавскаяЛюдмила/2Laba/Лаба 2 вариант 12/Лаба 2 вариант 12/Лаба 2 вариант 12.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    if (x == 0 && y == 0)
+        cout << 1;
+    else if (y == 0)
+        cout << 2;
+    else if (x == 0)
+        cout << 3;
+    else
+        cout << 0;
+
+    return 0;
+}
