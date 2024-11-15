1ed26150091d7ae1347bbad4028a147f0c0b3700
bobrovskayaanya <annabobrovskaa53gmail.com>
2024-11-15 10:22:12 +0300

    laba3.1

---
+++ БобровскаяАнна/laba3.1/laba3.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+double ans, n = 1 , k;
+int main()
+{
+
+    while (n>=1)
+	{
+		k = pow(-1, n + 1) * (1 / n);
+		if (abs(k) < 0.0001)
+			break;
+		ans = ans + k; 
+		n++;
+	}
+	cout << ans;
+}
