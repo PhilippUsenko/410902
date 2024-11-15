ab97e593a26b42f3904407db8771e8e6895b50f1
Elizaveta <sppunerizik@gmail.com>
2024-11-15 20:23:51 +0300

    LR 3
    laba 3

---
+++ ПлеховаЕлизавета/лаба 3/3.2/3.2/3.2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <iomanip> // для определеия точности результата
+#include <cmath> // для функции возведения в степень
+using namespace std;
+
+
+int main()
+
+{
+    int n, i=0;
+    double x = 0;
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите значение n" << endl;
+    cin >> n;
+
+    while (i <= n)
+    {
+        x = x + static_cast<double>(pow(-1, i) * (pow(2, i) / (pow(i + 1, i))));
+        i++;
+
+    }
+    cout << "Значение выражения равно " << fixed << setprecision(3) << x;
+
+    return 0;
+}
+
