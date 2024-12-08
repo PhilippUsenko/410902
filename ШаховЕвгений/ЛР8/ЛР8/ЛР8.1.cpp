fe503b4416f000e8208b97239390358c66439a23
Шахов Евгений <pelmesha047@gmail.com>
2024-12-08 20:15:52 +0300

    lr 1-8

---
+++ ШаховЕвгений/ЛР8/ЛР8/ЛР8.1.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <complex>
+
+using namespace std;
+
+
+bool proverka(int n, bool prov, int i) {
+	int proizv;
+	proizv = i * (i + 1) * (i + 2);
+	if (proizv == n) prov = true;
+	else prov = false;
+	return prov;
+}
+
+int sum(int a, int b) {
+	return a + b;
+}
+
+complex<double> sum(complex<double> c, complex<double> d) {
+	return c + d;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "Первое задание" << endl;
+	int n;
+	cin >> n;
+	bool prov = false;
+	int i = 0;
+	int proizv = 0;
+
+	while (proverka(n, prov, i) == false && (i * (i + 1) * (i + 2)) < n) {
+		i++;
+	}
+	if (proverka(n, prov, i) == true) {
+		cout << "Можно" << endl;
+	}
+	else {
+		cout << "Нельзя" <<endl;
+	}
+
+	cout << "Второе задание" << endl;
+	int a;
+	cin >> a;
+	int b;
+	cin >> b;
+
+	cout << sum(a, b) << endl;
+
+	complex<double> c(1.0, 2.5);
+	complex<double> d(3.5, 1.5);
+
+	complex<double> z;
+	z = sum(c, d);
+	cout << "z = " << z.real() << "+" << z.imag() <<"i"<< endl;
+
+	return 0;
+}
--- ШаховЕвгений/ОАиП ЛР7/ОАиП ЛР7/Source.cpp
+++ ШаховЕвгений/ОАиП ЛР7/ОАиП ЛР7/Source.cpp
@@ -1,37 +1,72 @@
 #include <iostream>
-#include <string>
-#include <stdio.h>
-#include <stdlib.h>
 #include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 
 using namespace std;
 
 
-class Stud{
-public: Stud(string fio, int group, double sr_bal, double dohod) : fio{ fio }, group{ group }, sr_bal{ sr_bal }, dohod{ dohod }{}
 
-	  void save() {
-		  cin >> fio >> group >> sr_bal >> dohod;
-		  ofstream out;
-		  out.open("students.txt");
-		  if (out.is_open()) {
-			  out << fio << ""
-		  }
-	  }
-	
+struct Student {
+	string fio;
+	string group;
+	double sr_bal;
+	double dohod;
+
+
+	void print(ostream& os) const {
+		os << fio << ", " << group << ", " << sr_bal << ", " << dohod << "\n";
+	}
 };
+
+bool priorety(const Student& a, const Student& b) {
+	const double mindohod = 626.0; // Минимальная зарплата
+	bool adohod = a.dohod < 2 * mindohod;
+	bool bdohod = b.dohod < 2 * mindohod;
+
+	if (adohod && !bdohod) return true; // a имеет приоритет
+	if (!adohod && bdohod) return false; // b имеет приоритет
+
+	return a.sr_bal > b.sr_bal;
+}
 
 int main() {
 
-	setlocale(LC_ALL, "Russian");
+	setlocale(LC_ALL, "Russian_Russia.1251");
+	vector <Student> students;
 
+	int num;
+	cout << "Введите колличество студентов: ";
+	cin >> num;
+	cin.ignore();
 
+	for (int i = 0; i < num; ++i) {
+		Student student;
+		cout << "Введите Ф.И.О. студента: ";
+		getline(cin, student.fio);
+		cout << "Введите группу: ";
+		getline(cin, student.group);
+		cout << "Введите средний балл: ";
+		cin >> student.sr_bal;
+		cout << "Введите доход на члена семьи: ";
+		cin >> student.dohod;
 
+		cin.ignore();
+		students.push_back(student);
+	}
 
+	sort(students.begin(), students.end(), priorety);
 
-	
+	ofstream out("students.txt", ios::app);
+	if (out.is_open()) {
+		for (const auto& student : students) {
+			student.print(out);
+		}
+	}
 
+	out.close();
 
 	return 0;
 }
