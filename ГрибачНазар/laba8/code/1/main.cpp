f39a8ad16bc203383fe1cf9a959f6e8705b5b354
N3zZa <nazar.developer007@gmail.com>
2024-12-11 20:21:40 +0300

    feat!:laba8

--- ГрибачНазар/laba8/code/1/main.cpp
+++ ГрибачНазар/laba8/code/1/main.cpp
@@ -1,34 +1,50 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
+// Функция для проверки, является ли число простым
 bool isPrimeNum(int num)
 {
     for (int i = 2; i * i <= num; i++)
     {
-        if (num % i == 0)
+        if (num % i == 0) // Если найден делитель, число не является простым
             return false;
     }
-    return true;
+    return true; // Если делителей нет, число простое
 }
 
-void setPrimeNums(vector<int> &primeNums, int P)
+// Функция для нахождения всех простых чисел от 2 до P
+void setPrimeNums(int *&primeNums, int &size, int P)
 {
+    int capacity = 10;             // начальный размер массива
+    primeNums = new int[capacity]; // выделяем память
+
     for (int i = 2; i <= P; i++)
     {
-        if (isPrimeNum(i))
+        if (isPrimeNum(i)) // Проверяем, является ли число простым
         {
-            primeNums.push_back(i);
+            if (size == capacity) // если массив заполнен
+            {
+                capacity *= 2;                 // увеличиваем размер
+                int *temp = new int[capacity]; // создаем новый массив большего размера
+                for (int j = 0; j < size; j++) // копируем данные
+                {
+                    temp[j] = primeNums[j];
+                }
+                delete[] primeNums; // освобождаем старый массив
+                primeNums = temp; // указываем указатель со старого массива на новый массив
+            }
+            primeNums[size++] = i; // добавляем простое число
         }
     }
 }
 
-void getNaturalNums(vector<int> &primeNums, int P)
+// Функция для вывода всех произведений пар простых чисел, не превышающих P
+void getNaturalNums(int *&primeNums, int &size, int P)
 {
-    for (int i = 0; i < primeNums.size(); i++)
+    for (int i = 0; i < size; i++)
     {
-        for (int j = i; j < primeNums.size(); j++)
+        for (int j = i; j < size; j++)
         {
             int numberValue = primeNums[i] * primeNums[j];
             if (numberValue <= P)
@@ -37,7 +53,7 @@ void getNaturalNums(vector<int> &primeNums, int P)
             }
         }
     }
-}
+}   
 
 int main()
 {
@@ -46,10 +62,12 @@ int main()
     cout << "Введите натуральное число: ";
     cin >> P;
 
-    vector<int> primeNums;
+    int *primeNums;           // Указатель на массив простых чисел
+    int size = 0;             // текущий размер массива простых чисел
 
-    setPrimeNums(primeNums, P);
-    getNaturalNums(primeNums, P);
+    setPrimeNums(primeNums, size, P);
+    getNaturalNums(primeNums, size, P);
 
+    delete[] primeNums; // освобождаем память
     return 0;
 }
--- ГрибачНазар/laba8/code/2/main.cpp
+++ ГрибачНазар/laba8/code/2/main.cpp
@@ -1,32 +1,41 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <numeric>
+#include <numeric> // Для функции lcm (НОК)
 
 using namespace std;
 
+// Функция для сложения десятичных дробей
 void solutionDouble(double num1, double num2, double &result)
 {
     result = num1 + num2;
 }
 
+// Функция для сложения обыкновенных дробей
 void solutionDouble(int nmtr1, int dmtr1, int nmtr2, int dmtr2)
 {
-    if (dmtr1 == dmtr2)
+    int nok = std::lcm(dmtr1, dmtr2); // Находим наименьшее общее кратное знаменателей
+    if (dmtr1 == dmtr2) // Если знаменатели одинаковы
     {
-        int resultNmtr = nmtr1 + nmtr2;
-        cout << resultNmtr << "/" << dmtr1;
+
+        if ((nmtr1 + nmtr2) == nok) // если сумма числителей равна ноку знаменателей
+        { 
+            int resultNmtr = (nmtr1 + nmtr2) / nok;
+            cout << "Результат: " << resultNmtr;
+        }
+        else
+        {
+            int resultNmtr = nmtr1 + nmtr2;
+            cout << resultNmtr << "/" << dmtr1;
+        }
     }
     else
     {
-        int nok = std::lcm(dmtr1, dmtr2);
-
+        // Приводим дроби к общему знаменателю
         int newNmtr1 = (nok / dmtr1) * nmtr1;
         int newNmtr2 = (nok / dmtr2) * nmtr2;
 
         int resultNmtr = newNmtr1 + newNmtr2;
 
-        if (resultNmtr % nok == 0) // если делится без остатка просто делю
+        if (resultNmtr % nok == 0) // если сумма числителей делится без остатка просто делю
         {
             int result = resultNmtr / nok;
             cout << result;
@@ -36,7 +45,7 @@ void solutionDouble(int nmtr1, int dmtr1, int nmtr2, int dmtr2)
         if (resultNmtr >= nok) // если числитель больше знаменателя произвожу преобразования
         {
             int chastnoe = resultNmtr / nok;
-            int raznost = resultNmtr - (nok * chastnoe);
+            int raznost = resultNmtr - (nok * chastnoe); // Остаток
 
             cout << "Не преобразованная дробь: " << resultNmtr << "/" << nok << endl;
 
@@ -63,13 +72,13 @@ int main()
         double result;
 
         cout
-            << "Введите первую дробь: ";
+            << "Введите первую десятичную дробь: ";
         cin >> num1;
 
-        cout << "Введите вторую дробь: ";
+        cout << "Введите вторую десятичную дробь: ";
         cin >> num2;
 
-        solutionDouble(num1, num2, result);
+        solutionDouble(num1, num2, result); // Вызываем функцию сложения десятичных дробей
 
         cout << "Ответ: " << result;
     }
@@ -93,7 +102,7 @@ int main()
         cout << "Введи знаменатель второй дроби: ";
         cin >> dmtr2;
 
-        solutionDouble(nmtr1, dmtr1, nmtr2, dmtr2);
+        solutionDouble(nmtr1, dmtr1, nmtr2, dmtr2); // Вызываем функцию сложения обыкновенных дробей
     }
 
     return 0;
